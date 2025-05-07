6ffcc871dc4a3e29170098c844cc3ee8857ff20e
Ahmad alomari <omariahmad571@gmail.com>
2025-05-07 10:34:38 +0300

    Persist state with localStorage, import Tailwind, clean code, and add reset button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import MyButton from "./MyButton";
 import MyInput from "./MyInput";
@@ -10,23 +8,22 @@ import SetMultipleTimes from "./SetMultipleTimes";
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <MyButton />
-      <MyInput />
-      <hr />
-      <MyForm />
-
-      <UsingArraysAsStateInTheComponent />
-
+      <div className="grid grid-cols-2 gap-4 my-4">
+        <MyButton />
+        <MyInput />
+        <MyForm />
+        <UsingArraysAsStateInTheComponent />
+      </div>
       <SetMultipleTimes />
+      <button
+        onClick={() => {
+          localStorage.clear();
+          window.location.reload();
+        }}
+      >
+        Reset
+      </button>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- src/UsingArraysAsStateInTheComponent.tsx
+++ src/UsingArraysAsStateInTheComponent.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 export default function UsingArraysAsStateInTheComponent() {
   const [divices, setDivices] = useState(["iphone1", "iphone2", "iphone3"]);
   const divicesList = divices.map((divice, index) => (
@@ -22,6 +22,7 @@ export default function UsingArraysAsStateInTheComponent() {
     const divicesNew = [...divices];
     divicesNew.splice(key, 1);
     setDivices(divicesNew);
+    localStorage.setItem("divices", JSON.stringify(divicesNew));
   }
   function handelEditItem(key: number) {
     const editedDevice = prompt(`Edit device number ${key + 1}`);
@@ -34,15 +35,22 @@ export default function UsingArraysAsStateInTheComponent() {
         return divice;
       });
       setDivices(devicesNew);
+      localStorage.setItem("divices", JSON.stringify(devicesNew));
     }
   }
-
   const [newDivice, setNewDivice] = useState("");
+  useEffect(() => {
+    const savedDivices = localStorage.getItem("divices");
+    setDivices(
+      savedDivices
+        ? JSON.parse(savedDivices)
+        : ["iphone1", "iphone2", "iphone3"]
+    );
+  }, []);
   return (
     <div
       style={{
         border: "1px solid #c43b3b",
-        marginTop: "10px",
       }}
     >
       <h3>Phones</h3>
@@ -57,8 +65,13 @@ export default function UsingArraysAsStateInTheComponent() {
         }
       />
       <button
+        className=" text-2xl p-0.5 m-1"
         onClick={() => {
-          setDivices(newDivice != "" ? [...divices, newDivice] : [...divices]);
+          setDivices(newDivice !== "" ? [...divices, newDivice] : [...divices]);
+          localStorage.setItem(
+            "divices",
+            JSON.stringify(divices.concat(newDivice))
+          );
           setNewDivice("");
         }}
       >
